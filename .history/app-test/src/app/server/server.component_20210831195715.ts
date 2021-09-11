2004d1b503478b976b27028121f2ce8da756052c
Lina Sachuk <sachuklina@gmail.com>
2021-09-10 18:55:44 -0600

    added style

--- .history/app-test/src/app/server/server.component_20210831195715.ts
+++ .history/app-test/src/app/server/server.component_20210831195715.ts
@@ -3,15 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styles:[
-    `.online {
-      color: white
-    }
-    `
-
-
-  ]
-
 })
 export class ServerComponent {
 serverId = 10;
