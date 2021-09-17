cc572e22e2bb6e496f93822e04d7a6969cd8c4d5
Lina Sachuk <sachuklina@gmail.com>
2021-09-17 14:09:41 -0600

    added recipes-detail

--- .history/angular-shopping-app/src/app/app.module_20210916222137.ts
+++ .history/angular-shopping-app/src/app/app.module_20210916222137.ts
@@ -5,11 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
@@ -19,8 +18,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
     RecipeListComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent,
-    RecipeDetailComponent
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
