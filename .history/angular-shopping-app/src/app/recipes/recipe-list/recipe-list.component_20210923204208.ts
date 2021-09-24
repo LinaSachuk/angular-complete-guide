5eaeea1a359c94200765a0df5ab17bcfc9438ba2
Lina Sachuk <sachuklina@gmail.com>
2021-09-23 21:06:32 -0600

    added test recipe

--- .history/angular-shopping-app/src/app/recipes/recipe-list/recipe-list.component_20210923204208.ts
+++ .history/angular-shopping-app/src/app/recipes/recipe-list/recipe-list.component_20210923204208.ts
@@ -7,9 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('A test recipe', "description for panna cotta ", "https://www.liveeatlearn.com/wp-content/uploads/2019/11/vegetarian-panna-cotta-vert-500x375.jpg")
-  ];
+  recipes:Recipe[] = [];
 
   constructor() { }
 
