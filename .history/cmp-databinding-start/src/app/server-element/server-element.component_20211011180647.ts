e1150578d358c744b4d8ec4b9c02669d9d80d541
Lina Sachuk <sachuklina@gmail.com>
2021-10-11 18:12:14 -0600

    added info for ngDoCHeck

--- .history/cmp-databinding-start/src/app/server-element/server-element.component_20211011180647.ts
+++ .history/cmp-databinding-start/src/app/server-element/server-element.component_20211011180647.ts
@@ -1,11 +1,11 @@
-import { Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit, OnChanges, DoCheck {
+export class ServerElementComponent implements OnInit, OnChanges {
 @Input("srvElement") element: {type:string, name: string, content: string}
  @Input() name: string;
 
@@ -20,8 +20,6 @@ ngOnChanges(changes: SimpleChanges): void {
   ngOnInit(): void {
     console.log('in the ngOnInit.')}
 
-ngDoCheck(){
-  console.log("ngDoCheck called.")
-}
+
 
 }
