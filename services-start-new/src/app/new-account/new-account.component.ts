8baf20a18fbe2d3477234d8553e5db462bd3e6f1
Lina Sachuk <sachuklina@gmail.com>
2022-02-04 18:11:10 -0700

    added injected service to component

--- services-start-new/src/app/new-account/new-account.component.ts
+++ services-start-new/src/app/new-account/new-account.component.ts
@@ -5,16 +5,18 @@ import { Loggingservice } from "../logging.service";
   selector: "app-new-account",
   templateUrl: "./new-account.component.html",
   styleUrls: ["./new-account.component.css"],
+  providers: [Loggingservice],
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
+
+  constructor(private loggingService: Loggingservice) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus,
     });
-    const service = new Loggingservice();
-    service.logStatusChange(accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
