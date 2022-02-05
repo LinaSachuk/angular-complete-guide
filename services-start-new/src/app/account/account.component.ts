00a72e7d6fb0d3ef1d8dadaf5019b5b86a0ae0e2
Lina Sachuk <sachuklina@gmail.com>
2022-02-05 13:31:44 -0700

    added accounts sercise

--- services-start-new/src/app/account/account.component.ts
+++ services-start-new/src/app/account/account.component.ts
@@ -1,24 +1,24 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { Loggingservice } from "../logging.service";
+import { AccountService } from "./account.service";
 
 @Component({
   selector: "app-account",
   templateUrl: "./account.component.html",
   styleUrls: ["./account.component.css"],
-  providers: [Loggingservice],
+  providers: [Loggingservice, AccountService],
 })
 export class AccountComponent {
   @Input() account: { name: string; status: string };
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{
-    id: number;
-    newStatus: string;
-  }>();
 
-  constructor(private loggingService: Loggingservice) {}
+  constructor(
+    private loggingService: Loggingservice,
+    private accountService: AccountService
+  ) {}
 
   onSetTo(status: string) {
-    this.statusChanged.emit({ id: this.id, newStatus: status });
     this.loggingService.logStatusChange(status);
+    this.accountService.updateStatus(this.id, status);
   }
 }
