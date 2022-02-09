8d9e9ae9827af26e0bd47d2fab558919e13a4253
Lina Sachuk <sachuklina@gmail.com>
2022-02-08 19:50:16 -0700

    injected service to service

--- services-start-new/src/app/new-account/new-account.component.ts
+++ services-start-new/src/app/new-account/new-account.component.ts
@@ -6,7 +6,7 @@ import { Loggingservice } from "../logging.service";
   selector: "app-new-account",
   templateUrl: "./new-account.component.html",
   styleUrls: ["./new-account.component.css"],
-  providers: [Loggingservice],
+  // providers: [Loggingservice],
 })
 export class NewAccountComponent {
   constructor(
@@ -15,7 +15,7 @@ export class NewAccountComponent {
   ) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
     this.accountService.addAccount(accountName, accountStatus);
   }
 }
