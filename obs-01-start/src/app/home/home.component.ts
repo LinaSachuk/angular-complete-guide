20161161261d63ee22b9f312ac085200f178f28c
Lina Sachuk <sachuklina@gmail.com>
2022-03-10 20:58:25 -0700

    added operators

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { interval, Observable, Subscription } from "rxjs";
+import { map, filter } from "rxjs/operators";
 
 @Component({
   selector: "app-home",
@@ -28,16 +29,26 @@ export class HomeComponent implements OnInit, OnDestroy {
         count++;
       }, 1000);
     });
-    this.firstObsSubscription = customIntervalObservable.subscribe(
-      (data) => {
-        console.log(data);
-      },
-      (error) => {
-        console.log(error);
-        alert(error.message);
-      },
-      () => console.log("Completed!")
-    );
+
+    this.firstObsSubscription = customIntervalObservable
+      .pipe(
+        filter((data) => {
+          return data > 0;
+        }),
+        map((data: number) => {
+          return "Round: " + (data + 1);
+        })
+      )
+      .subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (error) => {
+          console.log(error);
+          alert(error.message);
+        },
+        () => console.log("Completed!")
+      );
   }
 
   ngOnDestroy() {
