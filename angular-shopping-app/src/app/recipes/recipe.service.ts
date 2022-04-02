dcce568fd5f44cd136ef45a3e3e7405d1cdfa5ea
Lina Sachuk <sachuklina@gmail.com>
2022-04-01 20:52:33 -0600

    added onSave method

--- angular-shopping-app/src/app/recipes/recipe.service.ts
+++ angular-shopping-app/src/app/recipes/recipe.service.ts
@@ -2,9 +2,12 @@ import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
   private recipes: Recipe[] = [
     new Recipe(
       'A test recipe',
@@ -32,11 +35,21 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
   }
 
-  getRecipe(index: number) {
-    return this.recipes[index];
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
