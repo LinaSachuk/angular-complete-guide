ce8c15cbc1dbe4c5f5056de726d9a2d5d2283b50
Lina Sachuk <sachuklina@gmail.com>
2022-04-10 20:58:07 -0600

    added firebase server

--- angular-shopping-app/src/app/header/header/header.component.ts
+++ angular-shopping-app/src/app/header/header/header.component.ts
@@ -1,16 +1,25 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { DataStorageService } from '../../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-@Output() featureSelected = new EventEmitter<string>();
+  constructor(private dataStorageService: DataStorageService) {}
 
-  onSelect(feature: string){
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string) {
     this.featureSelected.emit(feature);
   }
 
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
 
+  onFetchData() {
+    this.dataStorageService.fetchRecipes();
+  }
 }
