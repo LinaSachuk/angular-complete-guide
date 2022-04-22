b97481ea6ab9f12b7720f039a4b356ee99d9afa1
Lina Sachuk <sachuklina@gmail.com>
2022-04-21 20:14:00 -0600

    added shared module

--- opt-mod-03-shared-module/src/app/app.module.ts
+++ opt-mod-03-shared-module/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-
 import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
@@ -25,7 +24,7 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule,
+    SharedModule
   ],
   providers: [
     ShoppingListService,
@@ -33,9 +32,9 @@ import { SharedModule } from './shared/shared.module';
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptorService,
-      multi: true,
-    },
+      multi: true
+    }
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
