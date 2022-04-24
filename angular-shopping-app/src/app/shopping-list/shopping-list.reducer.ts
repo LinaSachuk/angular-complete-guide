3173130ef4e8bce5e6e04181577ee74783514f79
Lina Sachuk <sachuklina@gmail.com>
2022-04-24 16:41:11 -0600

    added reducer

--- angular-shopping-app/src/app/shopping-list/shopping-list.reducer.ts
+++ angular-shopping-app/src/app/shopping-list/shopping-list.reducer.ts
@@ -1,3 +1,16 @@
-const initialState = {};
+import { Action } from 'rxjs/store';
+import { Ingredient } from '../shared/ingredient.model';
 
-export function ShoppingListReducer(state, action) {}
+const initialState = {
+  ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)],
+};
+
+export function ShoppingListReducer(state = initialState, action: Action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action],
+      };
+  }
+}
